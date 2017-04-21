760ef570dd647058a34055ae17f641f4c442017d
Steiner, Jerko <jerko.steiner@nyumc.org>
2017-04-21 18:56:45 -0400

    Add comparison

--- Hash.cs
+++ Hash.cs
@@ -1,11 +1,14 @@
 using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 using System.Text;
 using System.Web;
 using System;
 
 public class Hash {
 
-  private static string EncodeURL(string secret, string url) {
+  public static string REGEX_SIGNATURE_END = @"&signature=.*$";
+
+  public static string EncodeURL(string secret, string url) {
      // Encode the url and Convert the string to a byte array.
     string encodedURL = HttpUtility.UrlEncode(url.ToUpper());
     byte[] encodedUrlBytes = Encoding.UTF8.GetBytes(encodedURL.ToUpper());
@@ -15,20 +18,27 @@ public class Hash {
     byte[] hash = new HMACSHA1(sharedSecretBytes).ComputeHash(encodedUrlBytes);
     // Return the hexadecimal string.
     string localHash = BitConverter.ToString(hash).Replace("-", "");
-    url = String.Format("{0}&hash={1}", url, localHash);
+    url = String.Format(@"{0}&signature={1}", url, localHash);
     return url;
   }
 
   public static void Main(string[] args) {
     if (args.Length != 2) {
-      System.Console.Error.WriteLine("Usage: Hash.exe <secret> <url>");
+      System.Console.Error.WriteLine(@"Usage: Hash.exe <secret> <url>");
       return;
     }
 
-    string secret = args[0];
-    string url = args[1];
+    string secret = args[0].Trim();
+    string url = args[1].Trim();
 
+    string urlWithoutSignature = Regex.Replace(url, REGEX_SIGNATURE_END, "");
+    string encodedUrl = EncodeURL(secret, urlWithoutSignature);
 
-   https://www.new-innov.com/WebServices/NYUServices/NIIntegration.svc/UpdateEmail?kerberos=aao350&nyuemail=Arshed.Al-Obeidi@nyumc.org&consumerkey=HjEnOiWN7u8TEBTBVlV36wPtHpLt3SOI7pcPD2ljUW7BmNYzzXjhnHh5ezjI3Bw&timestamp=1490288604&signature=4C4B6054780B2CA821612C6716235BE0BD76C21B
+    System.Console.Error.WriteLine("");
+    System.Console.Error.WriteLine("url:          " + url);
+    System.Console.Error.WriteLine("");
+    System.Console.Error.WriteLine("encoded:      " + encodedUrl);
+    System.Console.Error.WriteLine("");
+    System.Console.Error.WriteLine("hash matches: " + url.Equals(encodedUrl));
   }
 }
